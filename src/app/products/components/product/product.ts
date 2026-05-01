b56865953e52d689a64f82b115dea6868cdebda1
Applexzs <reverseapplemw2@gmail.com>
2026-05-01 16:29:42 -0600

    Added create & update features

--- src/app/products/components/product/product.ts
+++ src/app/products/components/product/product.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product';
 import { Product } from '../../models/product';
+import { Form } from './form';
 
 
 @Component({
   selector: 'app-product',
-  imports: [],
+  imports: [Form],
   templateUrl: './product.html',
   styleUrl: './product.css',
 })
 export class ProductComponent implements OnInit{
   products: Product[] = [];
+
+  productSelected: Product = new Product();
+
   constructor(private service: ProductService){
   }
   
@@ -20,4 +24,24 @@ export class ProductComponent implements OnInit{
     })
   }
 
+  addProduct(product: Product) {
+    if(product.id > 0){
+      this.products = this.products.map(prod => {
+        if(prod.id == product.id){
+          return {... product}
+        }else{
+          return prod;
+        }
+      });
+    }else{
+      product.id = new Date().getTime();
+      this.products.push(product);
+    }
+
+    // this.products = [...this.products, {...product, id: new Date().getTime()}]
+  }
+
+  onUpdateProduct(product: Product) {
+    this.productSelected = product;
+  }
 }
