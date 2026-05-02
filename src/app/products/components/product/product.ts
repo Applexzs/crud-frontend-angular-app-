5e42ca585f7ec53c576dcb762c44c5dccea57f5e
Applexzs <reverseapplemw2@gmail.com>
2026-05-01 18:24:53 -0600

    Added backend connection

--- src/app/products/components/product/product.ts
+++ src/app/products/components/product/product.ts
@@ -26,23 +26,31 @@ export class ProductComponent implements OnInit{
 
   addProduct(product: Product): void {
     if(product.id > 0){
-      this.products = this.products.map(prod => {
-        if(prod.id == product.id){
-          return {... product}
-        }else{
-          return prod;
-        }
+      this.service.update(product).subscribe( productUpdated => {
+
+        this.products = this.products.map(prod => {
+          if(prod.id == product.id){
+            return {... product}
+          }else{
+            return prod;
+          }
+        });
       });
     }else{
-      product.id = new Date().getTime();
-      this.products.push(product);
+      this.service.create(product).subscribe(productNew => {
+        // product.id = new Date().getTime();
+        this.products.push(productNew);
+        // this.products = [...this.products, {...productNew}]
+      });
     }
     this.productSelected = new Product();
-    // this.products = [...this.products, {...product, id: new Date().getTime()}]
+    
   }
 
   onRemoveProduct(id: number): void{
-    this.products = this.products.filter(product => product.id != id);
+    this.service.remove(id).subscribe(() => {
+      this.products = this.products.filter(product => product.id != id);
+    });
   }
 
   onUpdateProduct(product: Product): void {
